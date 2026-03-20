f582434ce860ae59608c9e2d789f06ea9a11d8de
nsibille-investhub <nicolas.sibille@investhub.cloud>
2026-03-20 20:10:07 +0100

    Stabilize sheet open/close transitions for smoother slider animation

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -58,7 +58,7 @@ function SheetContent({
       <SheetPrimitive.Content
         data-slot="sheet-content"
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-4 shadow-lg transition-transform ease-[cubic-bezier(0.22,1,0.36,1)] data-[state=closed]:duration-300 data-[state=open]:duration-700 will-change-transform",
+          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-4 shadow-lg transition-[transform,opacity] data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:ease-out data-[state=closed]:ease-in will-change-transform",
           side === "right" &&
             "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full border-l w-[92vw] sm:w-[70vw] lg:w-[25vw] max-w-none",
           side === "left" &&
